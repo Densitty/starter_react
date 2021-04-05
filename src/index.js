e533b0cc5d70b39d25d27dc2ad0310f2f844ff3b
Densitty <dennis.brough@yandex.com>
2021-04-04 20:18:15 -0500

    introducing CSS methods and JS into our JSX

--- src/index.js
+++ src/index.js
@@ -1,24 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
+// to add CSS
+import "./index.css";
+
+function BookList() {
   return (
-    // nesting of components inside another component
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-// component_1
-const Person = () => {
-  return <h1>John Doe</h1>;
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+      {/* {} takes an expression and not a statement */}
+      {/* expressions are statements that return values */}
+      <p>This book is for children not below {4 + 2}</p>
+    </article>
+  );
 };
 
-// component_2
-function Message() {
-  return <p>I am a student at JS-U</p>;
-}
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/51rm2Y8L0ML._SX331_BO1,204,203,200_.jpg"
+      alt="book"
+    />
+  );
+};
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Author = () => {
+  const name = "Victoria Jamieson";
+  return (
+    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+      {name.toUpperCase()}
+    </h4>
+  );
+};
+
+const Title = () => {
+  return <h1>When Stars Are Scattered</h1>;
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
